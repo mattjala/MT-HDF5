b1bdab131e07c583bec74245c43caef69b4f2aa6
Matthew Larson <matthewjlar@gmail.com>
2025-02-11 10:21:28 -0600

    WIP: Remove leading slash in stored dset names

--- vol_bypass/H5VLbypass.c
+++ vol_bypass/H5VLbypass.c
@@ -1465,12 +1465,18 @@ dset_open_helper(void *obj, const char *name, H5VL_bypass_t *dset, hid_t dxpl_id
 
     /* The HDF5 library adds a '/' in front of the dataset name (full pathname).
      * Make sure the dataset name has it. */
+    // TBD
+    /*
     if (name) {
         if (name[0] != '/')
             sprintf(dset_stuff[dset_count].dset_name, "/%s", name);
         else
             strcpy(dset_stuff[dset_count].dset_name, name);
     }
+    */
+   if (name) {
+    strcpy(dset_stuff[dset_count].dset_name, name);
+   }
 
     /* Get the file name of the dataset */
     if (get_filename_helper(dset, file_name, H5I_DATASET, req) < 0) {
@@ -1630,7 +1636,7 @@ H5VL_bypass_dataset_open(void *obj, const H5VL_loc_params_t *loc_params, const c
          * store just the name or the full path from root.
          * With the current setup, it seems likely that multiple open requests to the same dataset from different
          * paths would not be recognized as the same dataset. */
-        if (!strcmp(dset_stuff[i].dset_name + 1, name)) {
+        if (!strcmp(dset_stuff[i].dset_name, name)) {
             dset_stuff[i].ref_count++;
 
             goto done;
@@ -3017,7 +3023,7 @@ H5VL_bypass_dataset_close(void *dset, hid_t dxpl_id, void **req)
     for (i = 0; i < dset_count; i++) {
         /* Skip leading slash in stored dset name */
         /* TBD: See note in dataset open about dataset name storage */
-        if (!strcmp(dset_stuff[i].dset_name + 1, dset_name)) {
+        if (!strcmp(dset_stuff[i].dset_name, dset_name)) {
             dset_stuff[i].ref_count--;
 
             if (dset_stuff[i].ref_count == 0)
