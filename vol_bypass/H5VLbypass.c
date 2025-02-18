0eb74f6723e5fdfbfc6e6efa3dce315af47ecd3c
Matthew Larson <matthewjlar@gmail.com>
2025-02-18 09:32:05 -0600

    Flush dataset before reads

--- vol_bypass/H5VLbypass.c
+++ vol_bypass/H5VLbypass.c
@@ -233,6 +233,9 @@ static herr_t get_dset_info(H5VL_bypass_t *dset, dset_t **info_out, hid_t dxpl_i
 static herr_t should_use_native(const dset_t *dset_info, bool *should_use_native);
 
 static herr_t remove_dset_info_helper(dset_t *dset_info);
+
+/* Flush the file containing the provided dataset */
+static herr_t flush_containing_file(H5VL_bypass_t *file);
 /*******************/
 /* Local variables */
 /*******************/
@@ -2470,8 +2473,17 @@ H5VL_bypass_dataset_read(size_t count, void *dset[], hid_t mem_type_id[], hid_t 
                 goto done;
             }
 
+            /* Future optimization: Only flush when a write has been performed*/
             /* Reset for the next H5Dread */
             pthread_mutex_lock(&mutex_local);
+            
+            if (flush_containing_file((H5VL_bypass_t *)dset[j]) < 0) {
+                fprintf(stderr, "failed to flush dataset\n");
+                ret_value = -1;
+                goto done;
+            }
+
+
             thread_task_finished = false;
             thread_loop_finish   = false;
             pthread_mutex_unlock(&mutex_local);
@@ -4893,6 +4905,25 @@ should_use_native(const dset_t *dset_info, bool *should_use_native) {
 
     // TBD: Link type?
     *should_use_native = false;
+
+done:
+    return ret_value;
+}
+
+/* Flush the file containing the provided dataset */
+static herr_t
+flush_containing_file(H5VL_bypass_t *dset) {
+    herr_t ret_value = 0;
+    H5VL_file_specific_args_t args;
+
+    args.args.flush.obj_type = H5I_DATASET;
+    args.args.flush.scope = H5F_SCOPE_LOCAL;
+
+    if ((H5VLfile_specific((void*) dset->under_object, dset->under_vol_id, &args, H5P_DEFAULT, NULL) < 0)) {
+        fprintf(stderr, "unable to flush file\n");
+        ret_value = -1;
+        goto done;
+    }
 
 done:
     return ret_value;
